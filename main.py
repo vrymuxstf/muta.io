dedced411c99df46ad5139ccb3856b246fe24a48
HoustonSCAV <laoxu@vrymux.com>
2024-06-13 14:32:29 -0400

    improve

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import uuid
+import uvicorn
+
+from fastapi import Depends, FastAPI, WebSocket, WebSocketDisconnect, Request, HTTPException
 from fastapi.responses import HTMLResponse
 
+from sqlalchemy.orm import Session
+
+from sqlalchemy import or_, and_
+
+import models
+from database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 class ConnectionManager:
@@ -24,6 +44,8 @@ manager = ConnectionManager()
 
 messages = []
 
+user_login_sessions = []
+
 
 @app.get("/")
 def read_root():
@@ -32,8 +54,85 @@ def read_root():
     return HTMLResponse(content)
 
 
+@app.get("/login")
+def login():
+    with open('login.html', 'r') as file:
+        content = file.read()
+    return HTMLResponse(content)
+
+
+@app.get("/client")
+def login():
+    with open('client.html', 'r') as file:
+        content = file.read()
+    return HTMLResponse(content)
+
+
+@app.get("/validation")
+def validation(token: str):
+    for user_login_session in user_login_sessions:
+        if user_login_session["token"] == token:
+            return True
+
+    return False
+
+
+@app.get("/create_user")
+def create_user(username: str, password: str, db: Session = Depends(get_db)):
+    db_user = models.User(username=username, password=password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+@app.get("/list_users")
+def list_users(token: str, db: Session = Depends(get_db)):
+    if validation(token):
+        return db.query(models.User).all()
+    else:
+        "Token not found!"
+
+    return db.query(models.User).all()
+
+
+@app.get("/list_user_login_sessions")
+def list_users():
+    return user_login_sessions
+
+
+@app.post("/login")
+def login(username: str, password: str, request: Request, db: Session = Depends(get_db)):
+    for user in db.query(models.User).filter(and_(models.User.username == username, models.User.password == password)):
+        user_login_session_token = str(uuid.uuid4())
+
+        user_login_sessions.append(
+            {
+                "username": user.username,
+                "token": user_login_session_token,
+                "ip": request.client.host,
+                "user_agent": request.headers.get("User-Agent"),
+            }
+        )
+
+        return user_login_session_token
+
+    raise HTTPException(status_code=401, detail="username or password incorrect")
+
+
+@app.get("get_username")
+def get_username(user_login_session_token: str):
+    for user_login_session in user_login_sessions:
+        if user_login_session["token"] == user_login_session_token:
+            return user_login_session["username"]
+
+    return ""
+
+
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket, user_login_session_token: str):
+    print(user_login_session_token)
+
     await manager.connect(websocket)
 
     for message in messages:
@@ -46,3 +145,7 @@ async def websocket_endpoint(websocket: WebSocket):
             await manager.broadcast(data)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app)
--- models.py
+++ models.py
@@ -1,10 +1,17 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 
 from database import Base
 
 
-class Message(Base):
-    __tablename__ = "usernames"
+class User(Base):
+    __tablename__ = "User"
 
-    username = Column(String, unique=True, index=True)
-    content = Column(String, index=True)
+    username = Column(String, unique=True, index=True, primary_key=True)
+    password = Column(String, index=True)
+
+
+class Message(Base):
+    __tablename__ = "Message"
+
+    username = Column(String, index=True, primary_key=True)
+    content = Column(String)
